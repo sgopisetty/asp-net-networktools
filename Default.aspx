3b673e4330058c486a9c0b73372659d75eb10a91
Sekhar Gopisetty <sekhar@spinutech.com>
2024-09-29 12:15:23 -0500

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <br/> MachineName:  <%= System.Environment.MachineName %>
+        <br/> UserName:  <%= System.Environment.UserName %>
+        <br/> LocalAddr:  <%= Request.ServerVariables["LOCAL_ADDR"] %>
+        <br/> RemoteAddr:  <%= Request.ServerVariables["REMOTE_ADDR"] %>
+        <br/> RemoteHost:  <%= Request.ServerVariables["REMOTE_HOST"] %>
+        <br /> IPv4 Local Address:  <asp:Label ID="lblIPv4Address" runat="server" />
+    </div>
+        <hr />
+        <div>
+            IP Ping: <asp:TextBox ID="tbRemoteIP" runat="server"></asp:TextBox>  <asp:Button ID="btnPing" runat="server" Text="Ping" OnClick="btnPing_Click" />
+            <asp:Label ID="lblPingResult" runat="server" />
+        </div>
+        <hr />
+        <div>
+            Traceroute: <asp:TextBox id="tbTraceRouteIP" runat="server"></asp:TextBox> <asp:Button ID="btnTraceRoute" runat="server" Text="TraceRoute" OnClick="btnTraceRoute_Click" />
+            <asp:Label ID="lblTraceRouteResult" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
+
+
---
+++ Default.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using System.Text;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblIPv4Address.Text = GetLocalIPv4();
+    }
+    private string GetLocalIPv4()
+    {
+        string localIPswithGateways = string.Empty;
+
+        var interfaces = NetworkInterface.GetAllNetworkInterfaces().Where(nic => nic.OperationalStatus == OperationalStatus.Up);
+        foreach (var nic in interfaces)
+        {
+            var ipProps = nic.GetIPProperties();
+
+
+            if (ipProps.GatewayAddresses.FirstOrDefault() != null)
+            {
+                var gateways = ipProps.GatewayAddresses.Select(g => g.Address).ToList();
+                if (gateways.Count() > 0)
+                {
+                    var ipv4 = ipProps.UnicastAddresses.FirstOrDefault(ip => ip.Address.AddressFamily == AddressFamily.InterNetwork);
+                    if (ipv4 != null)
+                    {
+                        localIPswithGateways += "<br/>-" + ipv4.Address.ToString();
+                    }
+                }
+            }
+        }
+        return localIPswithGateways;
+    }
+
+    protected void btnPing_Click(object sender, EventArgs e)
+    {
+        lblPingResult.Text = ExecutePing(tbRemoteIP.Text).Replace(Environment.NewLine, "<br />");
+    }
+
+    protected void btnTraceRoute_Click(object sender, EventArgs e)
+    {
+        lblTraceRouteResult.Text = ExecuteTraceRoute(tbTraceRouteIP.Text).Replace(Environment.NewLine, "<br />");
+    }
+
+    private string ExecuteTraceRoute(string ipAddress)
+    {
+        try
+        {
+            // Set up the process to execute the tracert command
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("tracert", "-h 4 " + ipAddress)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+
+                // Read the output of the tracert command
+                StringBuilder output = new StringBuilder();
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    output.AppendLine(process.StandardOutput.ReadLine());
+                }
+
+                return output.ToString();
+            }
+        }
+        catch (Exception ex)
+        {
+            return string.Format(@"Error: {0}", ex.Message);
+        }
+    }
+    private string ExecutePing(string ipAddress)
+    {
+        try
+        {
+            // Set up the process to execute the ping command
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("ping", ipAddress)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+
+                // Read the output of the ping command
+                StringBuilder output = new StringBuilder();
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    output.AppendLine(process.StandardOutput.ReadLine());
+                }
+
+                return output.ToString();
+            }
+        }
+        catch (Exception ex)
+        {
+            return string.Format(@"Error: {0}", ex.Message);
+        }
+    }
+}
